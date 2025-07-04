f803cd424fddfec1f68ac984b257fa274e24556b
SteveR <136388557+stevie912@users.noreply.github.com>
2025-07-04 19:13:20 +0000

    'reminders' page now displays table of all reminders in DB, with user_id, subject and timestamp

--- app/views/reminders/index.php
+++ app/views/reminders/index.php
@@ -5,7 +5,21 @@
             <div class="col-lg-12">
                 <h1>Reminders</h1>
                 <br>
-
+                <table class="table table-striped">
+                    <thead>
+                        <th>user ID</th>
+                        <th>Subject</th>
+                        <th>Date</th>
+                    </thead>
+                    <tbody>
+                        <?php foreach ($data['reminders_list'] as $reminder) { ?>
+                            <tr>
+                                <td><?php echo $reminder['user_id']; ?></td>
+                                <td><?php echo $reminder['subject']; ?></td>
+                                <td><?php echo $reminder['created_at']; ?></td>
+                            </tr>
+                        <?php } ?>
+                <!-- <?php print_r($data['reminders_list']); ?> -->
                 <br>
                 <p><a href="/reminders/create">Create Reminder</a></p>
             </div>
