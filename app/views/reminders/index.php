225a5f208bbb8cd34f0979282313301de164ca40
SteveR <136388557+stevie912@users.noreply.github.com>
2025-07-04 19:27:38 +0000

    added update and delete buttons to reminders table (not functional yet)

--- app/views/reminders/index.php
+++ app/views/reminders/index.php
@@ -5,27 +5,33 @@
             <div class="col-lg-12">
                 <h1>Reminders</h1>
                 <br>
-                <table class="table table-striped">
-                    <thead>
-                        <th>user ID</th>
-                        <th>Subject</th>
-                        <th>Date</th>
-                    </thead>
-                    <tbody>
-                        <?php foreach ($data['reminders_list'] as $reminder) { ?>
-                            <tr>
-                                <td><?php echo $reminder['user_id']; ?></td>
-                                <td><?php echo $reminder['subject']; ?></td>
-                                <td><?php echo $reminder['created_at']; ?></td>
-                            </tr>
-                        <?php } ?>
-                <!-- <?php print_r($data['reminders_list']); ?> -->
-                <br>
-                <p><a href="/reminders/create">Create Reminder</a></p>
             </div>
         </div>
     </div>
-
+    <div class="body-content"> 
+        <table class="table table-striped">
+            <thead>
+                <th>user ID</th>
+                <th>Subject</th>
+                <th>Date</th>
+                <th></th>
+                <th></th>
+            </thead>
+            <tbody>
+                <?php foreach ($data['reminders_list'] as $reminder) { ?>
+                    <tr>
+                        <td><?php echo $reminder['user_id']; ?></td>
+                        <td><?php echo $reminder['subject']; ?></td>
+                        <td><?php echo $reminder['created_at']; ?></td>
+                        <td><a href="/reminders/update/"</a>update</td>
+                        <td><a href="/reminders/delete/"</a>delete</td>
+                    </tr>
+                <?php } ?>
+            </tbody>
+        </table>
+        <br>
+        <p><a href="/reminders/create">Create Reminder</a></p>
+    </div>
    
-
     <?php require_once 'app/views/templates/footer.php' ?>
+</div>    
