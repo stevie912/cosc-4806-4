b077cb55bf45566d522b1716f0823b539bd92ea1
SteveR <136388557+stevie912@users.noreply.github.com>
2025-07-04 17:36:46 +0000

    Added basic structure to create new reminder

--- app/controllers/reminders.php
+++ app/controllers/reminders.php
@@ -5,9 +5,12 @@ class Reminders extends Controller {
   public function index() {
     $reminder = $this->model('Reminder');
     $reminders_list = $reminder->get_all_reminders();
-    print_r($reminders_list);
-    die;
-    $this->view('reminders/index');
+    $this->view('reminders/index', ['reminders_list' => $reminders_list]);
+  }
+
+  public function create() {
+    $reminder = $this->model('Reminder');
+    $this->view('reminders/create');
   }
 }
 ?>
--- app/views/reminders/create.php
+++ app/views/reminders/create.php
@@ -3,12 +3,12 @@
     <div class="page-header" id="banner">
         <div class="row">
             <div class="col-lg-12">
-                <h1>Reminders</h1>
-                <p><a href="/reminders/create">Create Reminder</a></p>
+                <h1>New Reminder</h1>
+                <button type="button" class="btn btn-primary" a href="/reminders/create">Create Reminder</button>
             </div>
         </div>
     </div>
 
-   
+
 
     <?php require_once 'app/views/templates/footer.php' ?>
